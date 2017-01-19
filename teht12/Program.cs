73cee5db810242eeef5c3e04f44e1e884c52ebe3
k8406 <k8406@student.labranet.jamk.fi>
2017-01-19 15:31:05 +0200

    teht12 in progress tbh fam

---
+++ teht12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numtable;
+            int num;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Gib number " + (i+1));
+                string str = Console.ReadLine();
+                num = int.Parse(str);
+                numtable[i] = num;
+            }
+
+            for (int j = 4; j < 5; j--)
+            {
+                Console.WriteLine(numtable[j]);
+            }
+        }
+    }
+}
