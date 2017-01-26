b03e53684ffb02fdc129b59ef14b4af389203201
k8406 <k8406@student.labranet.jamk.fi>
2017-01-26 12:59:32 +0200

    14 tehty fgt

---
+++ teht13/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, zero = 0, one = 0, two = 0, three = 0, four = 0, five = 0;
+            Console.WriteLine("The students vant grades blyat. Give grades cyka.");
+            do
+            {
+                string str = Console.ReadLine();
+                num = int.Parse(str);
+                if (num == 0)
+                {
+                    ++zero;
+                }
+                else if (num == 1)
+                {
+                    ++one;
+                }
+                else if (num == 2)
+                {
+                    ++two;
+                }
+                else if (num == 3)
+                {
+                    ++three;
+                }
+                else if (num == 4)
+                {
+                    ++four;
+                }
+                else if (num == 5)
+                {
+                    ++five;
+                }
+            } while (num != 6);
+
+            Console.WriteLine("Here's the fucking grades maggot.");
+            Console.Write("0: ");
+            for (int i = 0; i < zero; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+            Console.Write("1: ");
+            for (int i = 0; i < one; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+            Console.Write("2: ");
+            for (int i = 0; i < two; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+            Console.Write("3: ");
+            for (int i = 0; i < three; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+            Console.Write("4: ");
+            for (int i = 0; i < four; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+            Console.Write("5: ");
+            for (int i = 0; i < five; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
